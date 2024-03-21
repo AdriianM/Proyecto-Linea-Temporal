36d2bc2e32d59def577d734a541017f62377934c
Adrián Mora <adrianmoratoledo@hotmail.com>
2024-03-21 18:52:57 +0100

    actualización js y css con juan , ayuda con stefano

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+let listaEventos = [];
 
 // Función para seleccionar un elemento del DOM
 const Selector = (domItem) => document.querySelector(domItem);
@@ -7,15 +8,14 @@ const Selector = (domItem) => document.querySelector(domItem);
 const timeLine = Selector("#timeLine");
 
 // Realizar una petición fetch para obtener el archivo JSON "zelda-timeLine.json"
-fetch("zelda-timeline.json")
-  .then((response) => response.json()) // Convertir la respuesta a JSON
-  .then((jsonData) => {
-    // Ordenar los datos por fecha de forma ascendente
-    const jsonDataOrdenado = ordenarPorFechaAsc(jsonData);
-    // Mostrar los datos en formato HTML
-    mostrarJsonEnHTML(jsonDataOrdenado);
-  })
-  .catch((error) => console.error("Error cargando el JSON:", error)); // Capturar errores
+const fetchData = async () => {
+  try {
+    const response = await fetch("zelda-timeline.json");
+    listaEventos = await response.json();
+  } catch (error) {
+    console.error("Error cargando el JSON:", error);
+  }
+};
 
 // Función para ordenar los datos por fecha de forma ascendente
 const ordenarPorFechaAsc = (jsonData) => {
@@ -31,55 +31,43 @@ const ordenarPorFechaAsc = (jsonData) => {
 
 // Función para mostrar los datos en formato HTML
 function mostrarJsonEnHTML(jsonData) {
+  timeLine.innerHTML = "";
   const lista = document.createElement("ul");
   jsonData.forEach((obj) => {
     const item = document.createElement("li");
     // Crear elementos HTML con la información de cada objeto
     item.innerHTML = `
-          <p id="dateEvent">Date: ${obj.date}</p>
-          <p id="titleEvent">Title: ${obj.title}</p>
-          <img id="imgEvent" src="${obj.image}" alt="${obj.title}">
-          <p id="textEvent">${obj.text}</p>
+          <p class="dateEvent">Date: ${obj.date}</p>
+          <p class="titleEvent">Title: ${obj.title}</p>
+          <img class="imgEvent" src="${obj.image}" alt="${obj.title}">
+          <p class="textEvent">${obj.text}</p>
       `;
     lista.appendChild(item); // Añadir cada elemento a la lista
   });
   timeLine.appendChild(lista); // Añadir la lista al elemento con id "timeLine"
 }
 
-
 // Función para manejar el evento de agregar nuevo evento
 function handleAddEvent(e) {
   e.preventDefault();
   const date = Selector("#date").value;
   const title = Selector("#title").value;
-  const image = Selector("#image").value;
+  const image = Selector("#imageEvent").value;
   const text = Selector("#text").value;
-  
+
   const newEvent = {
-    date: date,
+    date: Number(date),
     title: title,
     image: image,
-    text: text
+    text: text,
   };
-  
+
   // Mostrar el nuevo evento en el timeline
-  mostrarNuevoEvento(newEvent);
+  listaEventos.push(newEvent);
+  listaEventos = ordenarPorFechaAsc(listaEventos);
+  console.log(listaEventos);
+  mostrarJsonEnHTML(listaEventos);
 }
-
-// Función para mostrar un nuevo evento en el timeline
-function mostrarNuevoEvento(event) {
-  const lista = timeLine.querySelector("ul");
-  const item = document.createElement("li");
-  // Crear elementos HTML con la información del nuevo evento
-  item.innerHTML = `
-      <p>Date: ${event.date}</p>
-      <p>Title: ${event.title}</p>
-      <img src="${event.image}" alt="${event.title}">
-      <p>Text: ${event.text}</p>
-  `;
-  lista.appendChild(item); // Añadir el nuevo evento a la lista existente
-}
-
 
 // Función para manejar el evento de mostrar/ocultar el formulario
 function btnForm() {
@@ -94,16 +82,13 @@ function btnForm() {
 // Agregar evento al botón para mostrar/ocultar el formulario
 Selector("#addBotonForm").addEventListener("click", btnForm);
 
-
-// Agregar evento al botón de agregar
-//Selector("#addEventBtn").addEventListener("click", handleAddEvent);
-
+Selector("#formContainer").addEventListener("submit", handleAddEvent);
 
 // Función principal para cargar los datos y mostrarlos en el timeline
 async function main() {
-  const jsonData = await fetchData();
-  const jsonDataOrdenado = ordenarPorFechaAsc(jsonData);
-  mostrarJsonEnHTML(jsonDataOrdenado);
+  await fetchData();
+  listaEventos = ordenarPorFechaAsc(listaEventos);
+  mostrarJsonEnHTML(listaEventos);
 }
 
 // Llamar a la función principal
